51c1e1f089819aaa07088981fb856598f773b85e
Liuy12 <yuanhangliu.ok@gmail.com>
2023-04-12 15:55:16 -0500

    add refdata_brain

--- R/data.R
+++ R/data.R
@@ -1,9 +1,9 @@
 #' Integrated scRNA-seq reference for breast tissue (subset)
-#' 
-#' Integrated scRNA-seq reference using random subset of two scRNA-seq datasets for breast tissue. One sample is from Komen tissue bank. 
+#'
+#' Integrated scRNA-seq reference using random subset of two scRNA-seq datasets for breast tissue. One sample is from Komen tissue bank.
 #' The other sample is from Gtex snRNA-seq data.
-#' 
-#' @format `refdata`
+#'
+#' @format
 #' a \code{\link[Seurat]{Seurat-class}} object with several metadata included:
 #' \describe{
 #'  \item{cellid}{cell barcodes}
@@ -11,8 +11,26 @@
 #'  \item{subjectid}{subject id}
 #'  \item{cohort}{data cohort, either "komentissuebank" or "gtex"}
 #' }
-#' 
-#' @source 
-#' <https://gtexportal.org/home/singleCellOverviewPage>
-#' <A single-cell atlas of the healthy breast tissues reveals clinically relevant clusters of breast epithelial cells>
+#'
+#' @source
+#' \url{https://gtexportal.org/home/singleCellOverviewPage}
+#' \url{https://pubmed.ncbi.nlm.nih.gov/33763657/}
 "refdata"
+
+
+
+#' Reference scRNA-seq data for mouse cortical cell taxonomy from the Allen Institute (subset)
+#'
+#' The data was generated using SMART-Seq2 protocol. 20 cells were randomly selected for each cell type. 15,000 highly variable
+#' genes were selected.
+#'
+#' @format
+#' a \code{\link[Seurat]{Seurat-class}} object with several metadata included:
+#' \describe{
+#'  \item{cellid}{cell barcodes}
+#'  \item{celltypes}{annotated cell types}
+#'  \item{subjectid}{subject id}
+#' }
+#'
+#' @source
+#' \url{https://www.nature.com/articles/nn.4216}
